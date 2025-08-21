7422d8f698be95e7d32d2e74e184a0bd25fa65a0
sekhar08 <sekhargandrotula@gmail.com>
2025-08-21 16:46:01 +0530

    added prisma to the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div>
-      <Button variant="secondary">Click me</Button>
-    </div>
-  );
+export default async function Home() {
+  return <div>Hello there</div>;
 }
