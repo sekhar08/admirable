756e59bf1a9e2fb68ab89db11db06394d5562ba0
sekhar08 <sekhargandrotula@gmail.com>
2025-08-26 13:03:04 +0530

    added inngest for background jobs

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,13 +1,29 @@
 import { z } from 'zod';
 import { baseProcedure, createTRPCRouter } from '../init';
+import { inngest } from '@/inngest/client';
 /**
  * This is the primary router for your server.
  *
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
+
+  invoke: baseProcedure
+    .input(
+      z.object({
+        text: z.string(),
+      })
+    )
+    .mutation(async ({ input }) => {
+      await inngest.send({
+        name: "test/hello.world",
+        data: { email: input.text }
+      })
+      return { ok: "success" }
+    }),
+
   hello: baseProcedure
-    .input (
+    .input(
       z.object({
         text: z.string(),
       }),
