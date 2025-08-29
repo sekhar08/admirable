32130ccb8fb32e50546ea77ca4d5f597f3e5f21e
sekhar08 <sekhargandrotula@gmail.com>
2025-08-29 11:17:33 +0530

    integrated sandbox environment

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,16 @@
 import { inngest } from "./client";
+import { Sandbox } from 'e2b'
 import { Agent, createAgent, anthropic } from "@inngest/agent-kit";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
     { id: "hello-world" },
     { event: "test/hello.world" },
-    async ({ event }) => {
+    async ({ event, step }) => {
+        const sandboxId = await step.run("get-sandbox-id", async () => {
+            const sandbox = await Sandbox.create("adorable-nextjs-test2")
+            return sandbox.sandboxId;
+        });
         const codeAgent = createAgent({
             name: "code-agent",
             system: "You are an expert nextjs developer. You write readable and maintainable code. You write simple NextJs & React Snippets",
@@ -20,6 +26,13 @@ export const helloWorld = inngest.createFunction(
         const { output } = await codeAgent.run(
             `Write the following snippet: ${event.data.value}`
         )
-        return output
+
+        const sandboxUrl = await step.run("get-sandbox-url", async () => {
+            const sandbox = await getSandbox(sandboxId);
+            const host = sandbox.getHost(3000);
+            return `https://${host}`;
+        });
+
+        return { output, sandboxUrl }
     },
 );
